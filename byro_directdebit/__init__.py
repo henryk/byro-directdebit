21ad6c999db04c6b9ec5bd1709b509a1080b156c
Henryk Plötz <henryk@ploetzli.ch>
2019-06-11 11:17:48 +0200

    Initial commit

---
+++ byro_directdebit/__init__.py
@@ -0,0 +1,20 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy
+
+
+class PluginApp(AppConfig):
+    name = 'byro_directdebit'
+    verbose_name = 'Byro SEPA Direct Debit plugin'
+
+    class ByroPluginMeta:
+        name = ugettext_lazy('Byro SEPA Direct Debit plugin')
+        author = 'Henryk Plötz'
+        description = ugettext_lazy('This plugin allows membership fees to be collected with SEPA direct debit')
+        visible = True
+        version = '0.0.1'
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
+default_app_config = 'byro_directdebit.PluginApp'
---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+from distutils.command.build import build
+
+from django.core import management
+from setuptools import setup, find_packages
+
+
+try:
+    with open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8') as f:
+        long_description = f.read()
+except:
+    long_description = ''
+
+
+class CustomBuild(build):
+    def run(self):
+        management.call_command('compilemessages', verbosity=1, interactive=False)
+        build.run(self)
+
+
+cmdclass = {
+    'build': CustomBuild
+}
+
+
+setup(
+    name='byro-directdebit',
+    version='0.0.1',
+    description='This plugin allows membership fees to be collected with SEPA direct debit',
+    long_description=long_description,
+    url='https://github.com/henryk/byro-directdebit',
+    author='Henryk Plötz',
+    author_email='henryk@ploetzli.ch',
+    license='Apache Software License',
+
+    install_requires=[],
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    cmdclass=cmdclass,
+    entry_points="""
+[byro.plugin]
+byro_directdebit=byro_directdebit:ByroPluginMeta
+""",
+)
