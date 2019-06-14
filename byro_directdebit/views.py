ed6470672c8a4156c7ef6219d643f28adb49fd16
Henryk Plötz <henryk@ploetzli.ch>
2019-06-14 12:22:34 +0200

    Better display

--- byro_directdebit/views.py
+++ byro_directdebit/views.py
@@ -1,10 +1,11 @@
 from collections import Counter
 
 from django.views.generic import ListView, TemplateView
+from django.db.models import Q
 
 from byro.members.models import Member
 from byro.plugins.sepa.models import SepaDirectDebitState
-
+from byro.bookkeeping.special_accounts import SpecialAccounts
 
 
 class MemberList(ListView):
@@ -54,42 +55,29 @@ class Dashboard(TemplateView):
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
 
-        all_members = list(Member.objects.all())
+        all_members = list(Member.objects.filter(
+            Q(memberships__amount__gt=0) |
+            Q(bookings__debit_account=SpecialAccounts.fees_receivable)
+        ).order_by('-id').distinct().all())
+
         with_due_balance = {
             member for member in all_members
             if member.balance < 0
         }
 
-        counts = Counter([m.profile_sepa.sepa_direct_debit_state for m in with_due_balance])
-        w_iban = sum(counts.values()) - counts[SepaDirectDebitState.NO_IBAN]
-        w_v_iban = w_iban - counts[SepaDirectDebitState.INVALID_IBAN]
-
-        w_rescinded = counts[SepaDirectDebitState.RESCINDED]
-        w_bounced = counts[SepaDirectDebitState.BOUNCED]
-        w_inactive = counts[SepaDirectDebitState.INACTIVE]
-
-        w_active = w_v_iban - w_rescinded - w_bounced - w_inactive
-
-        w_bic = w_active - counts[SepaDirectDebitState.NO_BIC]
-
-        w_mandate_r = w_bic - counts[SepaDirectDebitState.NO_MANDATE_REFERENCE]
-
-        assert w_mandate_r == counts[SepaDirectDebitState.OK]
+        counts = Counter([m.profile_sepa.sepa_direct_debit_state for m in all_members])
+        w_due_counts = Counter([m.profile_sepa.sepa_direct_debit_state for m in with_due_balance])
 
         context.update({
             'all_members': len(all_members),
             'with_due_balance': len(with_due_balance),
-            'with_iban': w_iban,
-            'with_valid_iban': w_v_iban,
-            'with_active_sepa': w_active,
-            'with_inactive_sepa': w_inactive,
-            'with_rescinded_sepa': w_rescinded,
-            'with_bounced_sepa': w_bounced,
-            'with_bic': w_bic,
-            'with_mandate_reference': w_mandate_r,
-            'without_mandate_reference': counts[SepaDirectDebitState.NO_MANDATE_REFERENCE],
-            'without_bic': counts[SepaDirectDebitState.NO_BIC],
-            'without_valid_iban': counts[SepaDirectDebitState.INVALID_IBAN],
+            'deactivated_sepa': w_due_counts[SepaDirectDebitState.INACTIVE],
+            'eligible': w_due_counts[SepaDirectDebitState.OK],
+            'invalid_iban': counts[SepaDirectDebitState.INVALID_IBAN],
+            'no_bic': counts[SepaDirectDebitState.NO_BIC],
+            'rescinded': counts[SepaDirectDebitState.RESCINDED],
+            'bounced': counts[SepaDirectDebitState.BOUNCED],
+            'no_mandate_reference': counts[SepaDirectDebitState.NO_MANDATE_REFERENCE],
         })
 
         return context
