129b847fb3c01d7810a7d6337d369cf54e304642
Henryk Plötz <henryk@ploetzli.ch>
2019-09-07 17:49:38 +0200

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ cmdclass = {
 
 setup(
     name='byro-directdebit',
-    version='0.0.1',
+    version='0.0.2',
     description='This plugin allows membership fees to be collected with SEPA direct debit',
     long_description=long_description,
     url='https://github.com/henryk/byro-directdebit',
