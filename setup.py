d6ac2f3e63391eb2d3741a9347c67d9cf9ec9308
Henryk Plötz <henryk@ploetzli.ch>
2019-09-23 09:10:26 +0200

    Bump SepaXML version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     author_email='henryk@ploetzli.ch',
     license='Apache Software License',
 
-    install_requires=['schwifty', 'workalendar', 'sepaxml==2.1.*'],
+    install_requires=['schwifty', 'workalendar', 'sepaxml==2.2.*'],
     packages=find_packages(exclude=['tests', 'tests.*']),
     include_package_data=True,
     cmdclass=cmdclass,
