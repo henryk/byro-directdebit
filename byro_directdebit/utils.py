07d305c1a08840e9adb431c89d7fef93933e2155
Henryk Plötz <henryk@ploetzli.ch>
2022-04-25 12:09:10 +0200

    Update for workalendar >= 10

--- byro_directdebit/utils.py
+++ byro_directdebit/utils.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 
 
 def next_debit_date(target_days=14, start_date=None, region="DE"):
-    cal = registry.get_calendar_class(region)()
+    cal = registry.get(region)()
     result = start_date or datetime.today()
     result = result + timedelta(days=target_days)
     result = cal.find_following_working_day(result)
